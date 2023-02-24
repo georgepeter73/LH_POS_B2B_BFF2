c65494e94801de25a1908efab99c43ecbcdb95dd
George Peter <george.peter@probots.in>
2023-02-24 14:44:55 -0500

    test

---
+++ src/main/java/com/lh/pos/bff/LhPosB2BBff2Application.java
@@ -0,0 +1,13 @@
+package com.lh.pos.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LhPosB2BBff2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LhPosB2BBff2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lh/pos/bff/LhPosB2BBff2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lh.pos.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LhPosB2BBff2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
